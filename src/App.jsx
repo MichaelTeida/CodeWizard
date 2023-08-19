31b8b44810d948810a54fec753cc31bc26a4f730
Michael <kontakt@ozum.pl>
2023-08-19 20:57:12 +0200

    add: header and SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
+import './styles/App.scss'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
 import Home from "./pages/Home.jsx"
+import PrimarySearchAppBar from "./components/Header.jsx";
 
 function App() {
     return (
         <>
+            <PrimarySearchAppBar/>
             <Home/>
         </>
     )
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,7 @@
 import {useForm} from "react-hook-form";
 import OpenAi from "../lib/open-ai.jsx";
 import React, {useState} from "react";
+import {Stack} from "@mui/material";
 
 function Chat() {
     const [prompt, setPrompt] = useState('')
@@ -19,24 +20,26 @@ function Chat() {
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <p>Prompt:</p>
-                <input
-                    type="text"
-                    placeholder="Prompt"
-                    {...register("prompt", {
-                        required: true,
-                        maxLength: 200,
-                    })}
-                />
-                <input type="submit" value="Wygeneruj"/>
-                {errors.prompt && (
-                    <div>
+                <Stack spacing={2} width={{p: 10}}>
+                    <p>Prompt:</p>
+                    <input
+                        type="text"
+                        placeholder="Prompt"
+                        {...register("prompt", {
+                            required: true,
+                            maxLength: 200,
+                        })}
+                    />
+                    <input type="submit" value="Wygeneruj" color="primary" variant="contained"/>
+                    {errors.prompt && (
+                        <div>
                         <span>
                             {errors.prompt.type === "required" && "Wymagane pole: Prompt"}
                             {errors.prompt.type === "maxLength" && "Maksymalna liczba słów: 200"}
                         </span>
-                    </div>
-                )}
+                        </div>
+                    )}
+                </Stack>
             </form>
             <div>
                 {prompt &&
@@ -48,3 +51,55 @@ function Chat() {
 }
 
 export default Chat;
+
+
+// import {useForm} from "react-hook-form";
+// import OpenAi from "../lib/open-ai.jsx";
+// import React, {useState} from "react";
+//
+// function Chat() {
+//     const [prompt, setPrompt] = useState('')
+//
+//     const {
+//         register,
+//         handleSubmit,
+//         formState: {errors},
+//     } = useForm();
+//
+//     const onSubmit = (data) => {
+//         console.log(data);
+//         setPrompt(data.prompt)
+//     };
+//
+//     return (
+//         <>
+//             <form onSubmit={handleSubmit(onSubmit)}>
+//                 <p>Prompt:</p>
+//                 <input
+//                     type="text"
+//                     placeholder="Prompt"
+//                     {...register("prompt", {
+//                         required: true,
+//                         maxLength: 200,
+//                     })}
+//                 />
+//                 <input type="submit" value="Wygeneruj"/>
+//                 {errors.prompt && (
+//                     <div>
+//                         <span>
+//                             {errors.prompt.type === "required" && "Wymagane pole: Prompt"}
+//                             {errors.prompt.type === "maxLength" && "Maksymalna liczba słów: 200"}
+//                         </span>
+//                     </div>
+//                 )}
+//             </form>
+//             <div>
+//                 {prompt &&
+//                     (<OpenAi prompt={prompt}/>)
+//                 }
+//             </div>
+//         </>
+//     );
+// }
+//
+// export default Chat;
